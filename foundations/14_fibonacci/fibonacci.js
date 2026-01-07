4a442af22966e52d1d239c50575c307bab45c3b9
KresiolAmu <bandojalouisemark@gmail.com>
2026-01-07 20:49:20 +0800

    adjusted /14_fibonacci/fibonacci.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,5 @@
 const fibonacci = function (num) {
-    num = parseInt(num);
+    num = parseInt(num) + 1;
     let accumulator = 1;
     let prevNum = 0;
     const arr = [];
@@ -9,16 +9,26 @@ const fibonacci = function (num) {
         return `OOPS`;
     }
 
+    // for (let i = 0; i < num; i++) {
+    //     arr[i] = accumulator;
+    //     accumulator += prevNum;
+    //     prevNum = accumulator - prevNum;
+    //     console.log(arr);
+    // }
+
     for (let i = 0; i < num; i++) {
-        arr[i] = accumulator;
-        accumulator += prevNum;
+        arr[i] = prevNum;
+        accumulator = accumulator + prevNum;
         prevNum = accumulator - prevNum;
+        // console.log(arr);
     }
 
-    return arr[arr.length - 1];
+    // console.log(arr);
+    // console.log(arr[num - 1]);
+    return arr[num - 1];
 };
 
-fibonacci(6)
+fibonacci(4);
 //[1, 1, 2]
 //[1, 2, 4|3]
 //[1|0, 2|1 ]
